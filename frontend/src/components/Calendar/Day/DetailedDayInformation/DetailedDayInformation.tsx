bd60830cccc349b4498e150f7dcc7d0890f8222c
rikobeast <jivkominchev.j@gmail.com>
2022-07-09 21:09:17 +0300

    Fix width for detailed expenses

--- frontend/src/components/Calendar/Day/DetailedDayInformation/DetailedDayInformation.tsx
+++ frontend/src/components/Calendar/Day/DetailedDayInformation/DetailedDayInformation.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 
 const DetailedDayInformation = () => {
   return (
-    <Card className="mx-2" width="700px">
-      <div className="flex flex-col p-1">
-        <h1 className="text-sm sm:text-lg p-3 font-bold">
-          Expenses for the selected day:
-        </h1>
-      </div>
-    </Card>
+    <div className="p-2 sm:p-0">
+      <Card className="sm:ml-2">
+        <div className="flex flex-col p-2">
+          <h1 className="text-sm text-lg text-center p-3 font-bold">
+            Expenses for the selected day
+          </h1>
+        </div>
+      </Card>
+    </div>
   );
 };
 
